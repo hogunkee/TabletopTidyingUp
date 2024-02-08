8fa7b21be35939a611e3806c41c756a9ce87af81
wooseoko <wooseoko@snu.ac.kr>
2024-02-08 12:51:26 +0900

    change some codes

--- train_test_split/complete/get_object_dict.py
+++ train_test_split/complete/get_object_dict.py
@@ -1,6 +1,6 @@
 import os
 
-split = 'test'
+split = 'train'
 categories = ['_'.join(c.split('_')[:-1]) for c in os.listdir() if os.path.isdir(c) and c.endswith(split)]
 category2obj = {}
 num_objects = 0
@@ -15,9 +15,10 @@ for cat in categories:
         num_objects += 1
 
 print(category2obj)
+print(len(category2obj.keys()))
 print('Total number of objects:', num_objects)
 
-with open('categories_dict_test.txt', 'w') as g:
+with open('categories_dict_train.txt', 'w') as g:
     g.writelines("{\n")
     for k in sorted(category2obj.keys()):
         g.writelines("\t'%s': "%k)
